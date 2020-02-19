0f0e359cacb8e413d06218966e7d6af098012d73
vincenzovolpe <vinci792010@gmail.com>
2020-02-19 18:49:29 +0100

    Migliorata tabella gestione post in admin

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12">
-                <h1 class="float-left">Lista di tutti i post per la gestione admin</h1>
+                <h1 class="float-left">Pagina Gestione post</h1>
                 <a class="btn btn-success float-right" href="{{ route('admin.posts.create') }}">
                     Crea nuovo post
                 </a>
@@ -12,6 +12,7 @@
         </div>
         <div class="row">
             <div class="col-sm-12">
+                <div class="table-responsive">
                 <table class="table">
                     <thead>
                         <tr>
@@ -19,7 +20,7 @@
                             <th>Titolo</th>
                             <th>Slug</th>
                             <th>Autore</th>
-                            <th>Azioni</th>
+                            <th class="text-right">Azioni</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -29,14 +30,14 @@
                                 <td>{{ $post->title }}</td>
                                 <td>{{ $post->slug }}</td>
                                 <td>{{ $post->author }}</td>
-                                <td>
+                                <td class="text-right">
+                                    <form class="" action="{{ route('admin.posts.destroy', ['post' => $post->id])}}" method="post">
                                     <a class="btn btn-info" href="{{ route('admin.posts.show', ['post' => $post->id ]) }}">
                                         Visualizza
                                     </a>
                                     <a class="btn btn-warning" href="{{ route('admin.posts.edit', ['post' => $post->id ]) }}">
                                         Modifica
                                     </a>
-                                    <form class="" action="{{ route('admin.posts.destroy', ['post' => $post->id])}}" method="post">
                                         @method('DELETE')
                                         @csrf
                                         <input class="btn btn-danger" type="submit" value="Cancella">
@@ -50,6 +51,7 @@
                         @endforelse
                     </tbody>
                 </table>
+                </div>
             </div>
         </div>
     </div>
